b67c206b0695518f27eec43aced18f483bda8020
alcat2008 <ge_yangyang@126.com>
2017-08-11 17:07:11 +0800

    docs: init project with nav tab added

---
+++ src/layout/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <img src="../assets/logo.png">
+
+    <ul>
+      <li>
+        <nav-tab href="/">Home</nav-tab>
+        <nav-tab href="/foo">Foo</nav-tab>
+        <nav-tab href="/bar">Bar</nav-tab>
+        <nav-tab href="/about">About</nav-tab>
+      </li>
+    </ul>
+
+    <slot></slot>
+  </div>
+</template>
+
+
+<script>
+  import NavTab from '../components/NavTab.vue'
+
+  export default {
+    components: {
+      NavTab
+    }
+  }
+</script>
+
+<style>
+.container {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main-layout>
+    <p>Welcome home</p>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layout/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
